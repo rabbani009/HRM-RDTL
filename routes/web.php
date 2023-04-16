47ba27e541eff360e7c2883cb03c759de72e0414
unknown <farzad.edsoft@gmail.com>
2023-04-16 22:19:22 +0600

    user-info-excel-imported

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use App\Http\Controllers\BackendControllers\{
     DepartmentController,
     EmployeeController,
     AjaxController,
-    CheckTimeController
+    CheckTimeController,
+    UserInfoController
 
 
 };
@@ -65,6 +66,10 @@ Route::group(['prefix' => 'backend', 'middleware' => 'authenticated'], function 
     //excel import routes.....
     Route::get('/checktime', [CheckTimeController::class, 'index'])->name('checktime.index');
     Route::post('/checktime', [CheckTimeController::class, 'store'])->name('checktime.store');
+
+      //excel UserInfoimport routes.....
+      Route::get('/userinfo', [UserInfoController::class, 'index'])->name('userinfo.index');
+      Route::post('/userinfo', [UserInfoController::class, 'store'])->name('userinfo.store');
 
 });
 
