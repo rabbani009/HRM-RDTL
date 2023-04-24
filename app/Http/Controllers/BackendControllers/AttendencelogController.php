d01457822cbeae50a933d47bfef43296e8b083d6
Fahim <fahimlxp@gmail.com>
2023-04-24 17:08:56 +0600

    24 april

--- app/Http/Controllers/BackendControllers/AttendencelogController.php
+++ app/Http/Controllers/BackendControllers/AttendencelogController.php
@@ -16,9 +16,9 @@ class AttendencelogController extends Controller
 
 public function index()
 {
-   
+
     // Retrieve data from the check_times and user_infos tables
-    
+
         $attendances = DB::table('check_times')
         ->join('user_infos', 'check_times.user_id', '=', 'user_infos.user_id')
         ->select('check_times.check_time', 'user_infos.user_id', 'user_infos.name')
@@ -33,7 +33,9 @@ public function index()
         $check = CheckTime::where('check_time', $attendance->check_time)
                     ->where('user_id', $attendance->user_id)
                     ->get();
+        //dd($check);
         if($check->isEmpty()){
+            dd($check);
             DB::table('attendance_logs')->insert([
                 'attend_date' => $attendance_date,
                 'user_id' => $attendance->user_id,
@@ -41,24 +43,36 @@ public function index()
                 'outtime' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
+
             ]);
         } else {
             $attendance_log = DB::table('attendance_logs')
                             ->where('user_id', $attendance->user_id)
                             ->where('attend_date', $attendance_date)
                             ->first();
+            //dd($attendance_log,$check,$attendance_date,$attendance->check_time);
             if($attendance_log){
+
                 $attendance_log_id = $attendance_log->id;
                 if(!$attendance_log->intime){
                     DB::table('attendance_logs')
                         ->where('id', $attendance_log_id)
                         ->update(['intime' => date('h:i A', strtotime($attendance->check_time)), 'updated_at' => now()]);
+                        dd("test");
                 } else if(!$attendance_log->outtime){
+
+                    $time = DB::table('check_times')
+                    ->where('user_id',$attendance->user_id)
+                    ->where('check_time','LIKE', '%'.$attendance_date.'%')
+                    ->max('check_time');
+
+
                     DB::table('attendance_logs')
                         ->where('id', $attendance_log_id)
-                        ->update(['outtime' => date('h:i A', strtotime($attendance->check_time)), 'updated_at' => now()]);
+                        ->update(['outtime' => date('h:i A', strtotime($time)), 'updated_at' => now()]);
                 }
-            } else {
+            }
+            else {
                 DB::table('attendance_logs')->insert([
                     'attend_date' => $attendance_date,
                     'user_id' => $attendance->user_id,
